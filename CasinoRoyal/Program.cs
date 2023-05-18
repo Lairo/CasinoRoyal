0019a20c620dd1daa266a1cd851bf36dcaa4cef4
chajus1 <chajus1@googlemail.com>
2023-05-18 12:14:01 +0100

    added player choice

--- CasinoRoyal/Program.cs
+++ CasinoRoyal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CasinoRoyal
 {
@@ -9,18 +10,40 @@ namespace CasinoRoyal
             Random random = new Random();
             double odds = .75;
 
-            Player player = new Player() { Cash = 100, Name = "The player" };
-            //Player Larry = new Player() { Cash = 100, Name = "Larry" };
+            Player player = new Player() { Cash = 100, Name = "Player #1" };
+            Player playerTwo = new Player() { Cash = 400, Name = "Player #2" };
+            Player playerThree = new Player() { Cash = 2000, Name = "Player #3" };
+
+            Player[] list = new Player[] { player, playerTwo, playerThree };
+            int selectedPlayer = 0;
+
+            //Console.WriteLine(list[1].Cash);
 
             while (true)
             {
-                if (player.Cash == 0)
+                foreach (var value in list)
+                {
+                    value.WriteMyInfo();
+                }
+
+                Console.WriteLine("Choose a player please 1,2 or 3");
+                string playerChoice = Console.ReadLine();
+                if (int.TryParse(playerChoice, out int choice)){
+                    selectedPlayer = choice - 1;
+                    break;
+                }
+            }
+            
+
+            while (true)
+            {
+                if (list[selectedPlayer].Cash == 0)
                 {
                     Console.WriteLine("The house always wins.\n");
                     return;
                 }
                 Console.WriteLine($"Welcome to the casino the odds are {odds}.");
-                player.WriteMyInfo();
+                list[selectedPlayer].WriteMyInfo();
 
                 Console.WriteLine("How much would you want to bet: ");
                 string howMuch = Console.ReadLine();
@@ -31,13 +54,13 @@ namespace CasinoRoyal
                     int pot = amount * 2;
                     if (chacne <= odds)
                     {
-                        player.GiveCash(amount);
+                        list[selectedPlayer].GiveCash(amount);
                         Console.WriteLine("Bad luck you lose.");
 
                     }
                     else
                     {
-                        player.ReceiveCash(pot);
+                        list[selectedPlayer].ReceiveCash(pot);
                         Console.WriteLine($"You win {pot}");
                     }
                 }
